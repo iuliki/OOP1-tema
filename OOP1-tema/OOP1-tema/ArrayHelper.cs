cba56759d9fd489b3f909e69b704c691754e110e
iuliki <iulia.bulugu@emanuel.ro>
2022-02-05 11:18:17 +0200

    rezolvare

---
+++ OOP1-tema/OOP1-tema/ArrayHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1_tema
+{
+    static  class ArrayHelper
+    {
+       public static int [] Fib(int n)
+        {
+            int[] array = new int[n];
+            array[0] = 0;
+            if (n == 0)
+                return array;
+
+            array[1] = 1;
+            if (n == 1)
+                return array;
+
+            int i = 1;
+            while(i<n-1)
+            {
+                i++;
+                array[i] = array[i - 1] + array[i - 2];
+
+            }
+            return array;
+
+
+        }
+
+        public static void Aparitii(int [] array,int element)
+        {
+            int nr = 0;
+            if(array!= null)
+            {
+                    for(int i=0;i<array.Length;i++)
+                                {
+                                    if (array[i] == element)
+                                        nr++;
+                                }
+            }
+            
+
+            Console.WriteLine($"elementul {element} apare de {nr} ori in vector ");
+        }
+
+        public static bool Identica(int[,] matrix)
+        {
+            bool isIdentica = true;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i != j)
+                    {
+                        if (matrix[i, j] != 0)
+                            isIdentica = false;
+                    }
+                    else
+
+                        if (matrix[i, j] != 1)
+                        isIdentica = false;
+
+                }
+            }
+            if (isIdentica == true)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
--- OOP1-tema/OOP1-tema/Program.cs
+++ OOP1-tema/OOP1-tema/Program.cs
@@ -7,6 +7,57 @@ namespace OOP1_tema
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("pana la ce numar sa se calculeze secventa fibonacii ;");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Calculul secventei fibonacci este: ");
+            int [] vector = ArrayHelper.Fib(n);
+
+            for(int j=0 ; j< vector.Length; j++)
+            {
+                
+                    Console.WriteLine(vector[j] + " ");
+            }
+
+
+            Console.WriteLine("ne de elemente din vector= ");
+            int nr = Int32.Parse(Console.ReadLine());
+            int[] array2 = new int[nr];
+
+            for(int i=0;i<array2.Length;i++)
+            {
+                Console.WriteLine($"elementul {array2[i]}=");
+                array2[i] = Int32.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("elementul a carui secventa vreau sa o vad este: ");
+            int numar = Int32.Parse(Console.ReadLine());
+            ArrayHelper.Aparitii(array2,numar);
+
+            Console.WriteLine("Numarul de linii: ");
+            int row = Int32.Parse(Console.ReadLine());
+           
+            int[,] matrix = new int[row, row];
+
+            for(int i=0;i<matrix.GetLength(0);i++)
+            {
+                for(int j=0;j<matrix.GetLength(1);j++)
+                {
+                    Console.WriteLine($"elemtul {matrix[i, j]}= ");
+                    matrix[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            if (ArrayHelper.Identica(matrix) == true)
+                Console.WriteLine("matricea este identica ");
+            else
+                Console.WriteLine("matricea nu este identica");
+
+
+
+
+
         }
     }
 }
